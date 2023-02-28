60067fb0c3b399061cacd47ab59b854996ada43c
Paulo Recce <reccepaulo@gmail.com>
2023-02-28 15:13:30 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let btn = document.querySelector("#boton-arriba");
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        btn.classList.add('visible');
+    }else{
+        btn.classList.remove('visible');
+    }
+});
+    
+btn.addEventListener("click", function() {
+    window.scrollTo(0, 0)
+});
+
+
